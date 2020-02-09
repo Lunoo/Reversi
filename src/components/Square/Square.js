6c1c4739b69b6ea3bdec1d72be9500bb2e0b445f
Roman <churpita.roman@gmail.com>
2020-02-09 21:57:06 +0300

    add initial state work on game rules

---
+++ src/components/Square/Square.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import styles from './Square.module.scss';
+import Disk from '../Disk/Disk';
+
+function Square(props) {
+    const classes = [styles.square];
+    if (props.isMarked) {
+        classes.push(styles.withMark);
+    }
+    if (props.isValidMove) {
+        classes.push(styles.validMove);
+    }
+    if (props.mayBeCaptured) {
+        classes.push(styles.mayBeCaptured);
+    }
+
+    return (
+        <div
+            className={classes.join(' ')}
+            onClick={props.clicked}
+            onMouseOver={props.hover}
+            onMouseLeave={props.blur}
+        >
+            {props.isNotEmpty && <Disk color={props.color}/>}
+        </div>
+    );
+}
+
+export default Square;
--- src/containers/Board/Board.js
+++ src/containers/Board/Board.js
@@ -1,49 +1,214 @@
 import React, {Component} from 'react';
 
-import Cell from '../../components/Cell/Сell'
 import styles from './Board.module.scss';
+import Square from '../../components/Square/Square'
+
+const BOARD_SIZE = 8;
 
 class Board extends Component {
     state = {
-        boardState: {}
+        squares: {
+            33: {
+                key: 33,
+                isNotEmpty: true,
+                color: 'white',
+                mayBeCaptured: false
+            },
+            44: {
+                key: 44,
+                isNotEmpty: true,
+                color: 'white',
+                mayBeCaptured: false
+            },
+            34: {
+                key: 34,
+                isNotEmpty: true,
+                color: 'black',
+                mayBeCaptured: false
+            },
+            43: {
+                key: 43,
+                isNotEmpty: true,
+                color: 'black',
+                mayBeCaptured: false
+            }
+        },
+        currentPlayer: 'black',
+        validMoves: [
+            32, 23, 54, 45
+        ]
     };
 
-    cellClicked = (key) => {
-        let newBoardState = {...this.state.boardState};
-        let newCell = newBoardState[key];
-        newBoardState[key] = {
-            isNotEmpty: true,
-            color: newCell?.color === 'white' ? 'black' : 'white'
-        };
+    isSquareNotEmpty = (key) => {
+        return !!this.state.squares[key]?.isNotEmpty;
+    };
 
-        this.setState({boardState: newBoardState});
-        console.log(key, newBoardState);
+    getAllValidMoves = (boardState) => {
+        const validMoves = [];
+        for (let y = 0; y < BOARD_SIZE; y++) {
+            for (let x = 0; x < BOARD_SIZE; x++) {
+                const key = this.getKey(x, y);
+
+                if (this.isSquareNotEmpty(key)) {
+                    continue;
+                }
+
+                const neighbors = this.getAllNeighborsWithColor(boardState, key, this.state.currentPlayer);
+                const capturedDisks = this.getAllCapturedDisks(boardState, key, neighbors);
+
+                if (capturedDisks.length > 0) {
+                    validMoves.push(key);
+                }
+            }
+        }
+
+        return validMoves;
+    };
+
+
+    getAllCapturedDisks = (boardState, key, neighbors) => {
+        let capturedDisks = [];
+        neighbors.forEach((square) => {
+            const vector = square.key - key;
+            const disks = [];
+
+            let squareToCheck = square;
+            while (squareToCheck && squareToCheck.color === square.color) {
+                disks.push({...squareToCheck});
+                squareToCheck = boardState[squareToCheck.key + vector];
+            }
+
+            if (squareToCheck) {
+                capturedDisks = [
+                    ...capturedDisks,
+                    ...disks
+                ]
+            }
+        });
+
+        return capturedDisks;
+    };
+
+    getAllNeighborsWithColor = (boardState, key, color) => {
+        const neighbors = [];
+        for (let y = -1; y <= 1; y++) {
+            for (let x = -1; x <= 1; x++) {
+                const vector = this.getKey(x, y);
+                const square = boardState[key + vector];
+                if (square && square.key !== key && square.color === color) {
+                    neighbors.push({...square})
+                }
+            }
+        }
+
+        return neighbors;
     };
 
     getKey = (x, y) => {
-        return x * 100 + y;
+        return x * 10 + y;
+    };
+
+    getNextPlayer = (currentPlayer) => {
+        return currentPlayer === 'black' ? 'white' : 'black';
+    };
+
+    showCapturedDisks = (key) => {
+        if (this.isSquareNotEmpty(key)) {
+            return;
+        }
+
+        const newBoardState = {...this.state.squares};
+        const newColor = this.getNextPlayer(this.state.currentPlayer);
+        const neighbors = this.getAllNeighborsWithColor(newBoardState, key, newColor);
+        const squaresToBeCaptured = this.getAllCapturedDisks(newBoardState, key, neighbors);
+
+        if (!squaresToBeCaptured.length) {
+            return;
+        }
+
+        squaresToBeCaptured.forEach((square) => {
+            newBoardState[square.key].mayBeCaptured = true;
+        });
+
+        this.setState({
+            squares: newBoardState
+        });
+    };
+
+    hideCapturedDisks = () => {
+        const newBoardState = {...this.state.squares};
+        Object.values(newBoardState).forEach((square) => {
+            const newSquare = {...square};
+            newSquare.mayBeCaptured = false;
+            newBoardState[square.key] = newSquare;
+        });
+
+        this.setState({
+            squares: newBoardState
+        });
+    };
+
+    squareClicked = (key) => {
+        if (!this.state.validMoves.includes(key)) {
+            return;
+        }
+
+        const newColor = this.getNextPlayer(this.state.currentPlayer);
+        const neighbors = this.getAllNeighborsWithColor(this.state.squares, key, newColor);
+        const capturedDisks = this.getAllCapturedDisks(this.state.squares, key, neighbors);
+
+        // Add new disk
+        const newBoardState = {...this.state.squares};
+        newBoardState[key] = {
+            key: key,
+            isNotEmpty: true,
+            color: this.state.currentPlayer
+        };
+
+        // Change color of captured disks
+        capturedDisks.forEach((disk) => {
+            newBoardState[disk.key].color = this.state.currentPlayer;
+            newBoardState[disk.key].mayBeCaptured = false;
+        });
+
+        // update allowable squares for the next player
+        const validMoves = this.getAllValidMoves(newBoardState);
+
+        this.setState((prevState) => {
+            return {
+                currentPlayer: this.getNextPlayer(prevState.currentPlayer),
+                squares: newBoardState,
+                validMoves: validMoves
+            }
+        });
     };
 
     render() {
-        const cells = [];
-        for (let y = 0; y < 8; y++) {
-            for (let x = 0; x < 8; x++) {
+        const squares = [];
+        for (let y = 0; y < BOARD_SIZE; y++) {
+            for (let x = 0; x < BOARD_SIZE; x++) {
                 const key = this.getKey(x, y);
-                const cellState = this.state.boardState[key];
-                cells.push(
-                    <Cell isMarked={[2, 6].includes(x) && [2, 6].includes(y)}
-                          key={key}
-                          isNotEmpty={cellState?.isNotEmpty}
-                          color={cellState?.color}
-                          clicked={() => this.cellClicked(key)}
+                const squareState = this.state.squares[key];
+
+                squares.push(
+                    <Square isMarked={[2, 6].includes(x) && [2, 6].includes(y)}
+                            key={key}
+                            isValidMove={this.state.validMoves.includes(key) ? this.state.currentPlayer : null}
+                            isNotEmpty={squareState?.isNotEmpty}
+                            mayBeCaptured={squareState?.mayBeCaptured}
+                            color={squareState?.color}
+                            hover={() => this.showCapturedDisks(key)}
+                            blur={() => this.hideCapturedDisks(key)}
+                            clicked={() => this.squareClicked(key)}
                     />
                 )
             }
         }
 
         return (
-            <div className={styles.board}>
-                {cells}
+            <div className={styles.board}
+                 data-player={this.state.currentPlayer}>
+                {squares}
             </div>
         );
     }
