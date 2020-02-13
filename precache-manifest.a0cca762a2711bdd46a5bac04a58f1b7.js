127481e343a47a4cc36d713493d4b1304d0d079c
Roman <churpita.roman@gmail.com>
2020-02-13 20:45:31 +0300

    Updates

---
+++ precache-manifest.a0cca762a2711bdd46a5bac04a58f1b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c316986a97fa36a938f9230401e30dcf",
+    "url": "/Reversi/index.html"
+  },
+  {
+    "revision": "b435ccee5933a676a698",
+    "url": "/Reversi/static/css/main.22673c63.chunk.css"
+  },
+  {
+    "revision": "0dc254d236fe5752e749",
+    "url": "/Reversi/static/js/2.e78f1ef1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Reversi/static/js/2.e78f1ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b435ccee5933a676a698",
+    "url": "/Reversi/static/js/main.9093ae6e.chunk.js"
+  },
+  {
+    "revision": "388335abe209b548f7ff",
+    "url": "/Reversi/static/js/runtime-main.72b3cd6b.js"
+  },
+  {
+    "revision": "ac610868b37d41abcec427d4cdba3200",
+    "url": "/Reversi/static/media/wood-bg.ac610868.jpg"
+  }
+]);
