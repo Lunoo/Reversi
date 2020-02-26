5d5a6fdaedc9c2e302324fd63dcd0b100a1e7d0c
r.churpita <r.churpita@godeltech.com>
2020-02-26 15:23:16 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, {Component} from 'react';
 
-import './App.scss';
+import styles from './App.module.scss';
 import Header from './containers/Header/Header';
 import Board from './containers/Board/Board';
 import UserPanel from './containers/UserPanel/UserPanel';
 
-const BG_CLASSES = ['green']; // ['blue', 'green']
-
 class App extends Component {
-    getRandomItemFromArray = (arr) => {
-        const index = Math.floor(Math.random() * arr.length);
-        return arr[index];
-    };
-
     render() {
-        document.body.classList.add(this.getRandomItemFromArray(BG_CLASSES));
         return (
-            <div className="app">
+	        <div className={[styles.app, styles.green].join(' ')}>
                 <Header/>
                 <Board/>
                 <UserPanel/>
