ae6fdc6e1c650c465072408574fceda241d0b58f
Roman <churpita.roman@gmail.com>
2020-02-29 18:02:06 +0300

    fix PlayerCard.js styles

--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -3,25 +3,19 @@ import {CSSTransition} from 'react-transition-group';
 
 import styles from './PlayerCard.module.scss';
 
-const PlayerCard = props => {
-    const classes = [styles.playerCard, styles[props.color]];
-    if (props.active) {
-        classes.push(styles.active);
-    }
-
-    return (
-        <CSSTransition
-            in={props.show}
-            timeout={300}
-            classNames={{...styles}}
-            mountOnEnter
-            unmountOnExit>
-            <figure className={classes.join(' ')}>
-                <img src={require(`../../assets/images/${props.icon}`)} alt={props.color}/>
-                <figcaption>{props.nickname}</figcaption>
-            </figure>
-        </CSSTransition>
-    );
-};
+const PlayerCard = props => (
+    <CSSTransition
+        in={props.show}
+        timeout={300}
+        classNames={{...styles}}
+        mountOnEnter
+        unmountOnExit>
+        <figure className={styles.playerCard + ' ' + styles[props.color]}>
+            <div className={props.active ? styles.active : ''}/>
+            <img src={require(`../../assets/images/${props.icon}`)} alt={props.color}/>
+            <figcaption>{props.nickname}</figcaption>
+        </figure>
+    </CSSTransition>
+);
 
 export default PlayerCard;
